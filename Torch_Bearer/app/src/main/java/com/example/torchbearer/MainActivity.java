ca5055ee9a3b9d0423a43978e4d860d984dffca2
Shijie Liu <liushijie@Shijies-MacBook-Pro.local>
2021-12-04 17:33:55 -0800

    google map added

--- Torch_Bearer/app/src/main/java/com/example/torchbearer/MainActivity.java
+++ Torch_Bearer/app/src/main/java/com/example/torchbearer/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.torchbearer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
     }
 }
