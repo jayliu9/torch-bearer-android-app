fbd9d5816becdabc23b9d5b37219587b75235585
Shijie Liu <liushijie@Shijies-MacBook-Pro.local>
2021-12-04 22:47:32 -0800

    gps done

--- Torch_Bearer/app/src/main/java/com/example/torchbearer/MainActivity.java
+++ Torch_Bearer/app/src/main/java/com/example/torchbearer/MainActivity.java
@@ -2,31 +2,134 @@ package com.example.torchbearer;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 
     GoogleMap map;
 
+    private DatabaseReference reference;
+    private LocationManager manager;
+
+    private final int MIN_TIME = 1000;
+    private final int MIN_DISTANCE = 1;
+
+    Marker myMaker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FirebaseDatabase.getInstance().getReference().setValue("This is Torch Bearer");
+        reference = FirebaseDatabase.getInstance().getReference().child("User-101");
+        manager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        //FirebaseDatabase.getInstance().getReference().setValue("This is Torch Bearer");
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        getLocationUpdate();
+
+        readChanges();
+    }
+
+    private void readChanges() {
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    try {
+                        MyLocation location = snapshot.getValue(MyLocation.class);
+                        if (location != null) {
+                            myMaker.setPosition(new LatLng(location.getLatitude(), location.getLongitude()));
+                        }
+                    } catch (Exception e) {
+                        Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+
+    private void getLocationUpdate() {
+        if (manager != null) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                if (manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                    manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME, MIN_DISTANCE, this);
+                } else if (manager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+                    manager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME, MIN_DISTANCE, this);
+                } else {
+                    Toast.makeText(this, "No Provider Enabled", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 101);
+            }
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 101) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLocationUpdate();
+            } else {
+                Toast.makeText(this, "Permission Required", Toast.LENGTH_SHORT).show();
+            }
+
+        }
     }
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
+        map = googleMap;
+
+        LatLng seattle = new LatLng(47, -122);
+        myMaker = map.addMarker(new MarkerOptions().position(seattle).title("Marker"));
+        map.getUiSettings().setZoomControlsEnabled(true);
+        map.getUiSettings().setAllGesturesEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLng(seattle));
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        if (location != null) {
+            saveLocation(location);
+        } else {
+            Toast.makeText(this, "No location", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void saveLocation(Location location) {
+        reference.setValue(location);
     }
 }
