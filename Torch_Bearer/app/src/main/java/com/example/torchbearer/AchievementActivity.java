e99510dedd90d8aa0d4f964c60a6ef44a51c891c
ziyingjin94 <ziyingjin94@gmail.com>
2021-12-05 16:46:41 -0800

    created achievement fragments and items

--- Torch_Bearer/app/src/main/java/com/example/torchbearer/AchievementActivity.java
+++ Torch_Bearer/app/src/main/java/com/example/torchbearer/AchievementActivity.java
@@ -1,8 +1,14 @@
 package com.example.torchbearer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class AchievementActivity extends AppCompatActivity {
 
@@ -10,5 +16,29 @@ public class AchievementActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_achievement);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.achievement_navigation);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selected = null;
+                switch(item.getItemId()) {
+                    case R.id.achieved:
+                        selected = new AchievedFragment();
+                        break;
+                    case R.id.achievable:
+                        selected = new AchievableFragment();
+                        break;
+                }
+                if (selected != null) {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.achievement_fragment_container, selected).commit();
+                }
+                return true;
+            }
+        });
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.achievement_fragment_container, new AchievedFragment()).commit();
     }
 }
