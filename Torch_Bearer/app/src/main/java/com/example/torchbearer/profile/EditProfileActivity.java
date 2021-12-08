d02447f437aff7ce20356d3a709bf72a88c05575
ziyingjin94 <ziyingjin94@gmail.com>
2021-12-07 16:33:02 -0800

    profile xml design

--- Torch_Bearer/app/src/main/java/com/example/torchbearer/profile/EditProfileActivity.java
+++ Torch_Bearer/app/src/main/java/com/example/torchbearer/profile/EditProfileActivity.java
@@ -1,14 +1,16 @@
-package com.example.torchbearer;
+package com.example.torchbearer.profile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ProfileActivity extends AppCompatActivity {
+import com.example.torchbearer.R;
+
+public class EditProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.activity_edit_profile);
     }
 }
---
+++ Torch_Bearer/app/src/main/java/com/example/torchbearer/profile/ProfileActivity.java
@@ -0,0 +1,16 @@
+package com.example.torchbearer.profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.torchbearer.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
