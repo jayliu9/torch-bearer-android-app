cea2776df932e730153df03a54b75b1cf33499ff
ziyingjin94 <ziyingjin94@gmail.com>
2021-12-08 00:23:01 -0800

    set profile pic

--- Torch_Bearer/app/src/main/java/com/example/torchbearer/profile/ProfileActivity.java
+++ Torch_Bearer/app/src/main/java/com/example/torchbearer/profile/ProfileActivity.java
@@ -1,16 +1,50 @@
 package com.example.torchbearer.profile;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.torchbearer.R;
 
 public class ProfileActivity extends AppCompatActivity {
-
+    private ImageView profileImage;
+    private Uri imageUri;
+    private ActivityResultLauncher<Intent> activityResultLauncher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        profileImage = findViewById(R.id.imageview_profile);
+        profileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                choosePicture();
+            }
+        });
+    }
+
+    private void choosePicture() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts
+                .StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
+                    imageUri = result.getData().getData();
+                    profileImage.setImageURI(imageUri);
+                }
+            }
+        });
     }
 }
