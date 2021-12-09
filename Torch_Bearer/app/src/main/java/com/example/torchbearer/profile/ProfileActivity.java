51dd180592ec5c9decc33198d9a8566ff478d956
ziyingjin94 <ziyingjin94@gmail.com>
2021-12-08 22:05:06 -0800

    Edit Profile activity design

--- Torch_Bearer/app/src/main/java/com/example/torchbearer/profile/ProfileActivity.java
+++ Torch_Bearer/app/src/main/java/com/example/torchbearer/profile/ProfileActivity.java
@@ -11,14 +11,16 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.example.torchbearer.R;
 
 public class ProfileActivity extends AppCompatActivity {
     private ImageView profileImage;
+    private ImageView editProfileImage;
     private Uri imageUri;
-    private ActivityResultLauncher<Intent> activityResultLauncher;
+    private Button editProfileButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,13 +32,34 @@ public class ProfileActivity extends AppCompatActivity {
                 choosePicture();
             }
         });
+        editProfileImage = findViewById(R.id.edit_profile_image);
+        editProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                choosePicture();
+            }
+        });
+
+        editProfileButton = findViewById(R.id.button_edit_profile);
+        editProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startEditProfileActivity();
+            }
+        });
+
+    }
+
+    private void startEditProfileActivity() {
+        Intent intent = new Intent(this, EditProfileActivity.class);
+        startActivity(intent);
     }
 
     private void choosePicture() {
         Intent intent = new Intent();
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
-        activityResultLauncher = registerForActivityResult(new ActivityResultContracts
+        ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(new ActivityResultContracts
                 .StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
             @Override
             public void onActivityResult(ActivityResult result) {
